03d4a6ba771678e73b3103966c73cf6be4f4126a
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-11 21:02:08 +0200

    react components

--- FirstExample/scripts/App.js
+++ FirstExample/scripts/App.js
@@ -1,9 +1,57 @@
-class CheckBoxConfirmation extends React.Component{
-    redner(){
-        return(
-            <h1>Kup bilet na horror roku!</h1>
-            <input type="checkbox"/>
-        <label hmtlFor=""></label>
+const PositiveMessage = () => <p>Możesz obejrzeć film. Zapraszamy!</p>;
+const NegativeMessage = () => <p>Nie możesz obejrzeć filmów, ponieważ masz mniej niż 16 lat!</p>;
+
+
+class TicketShop extends React.Component {
+
+    state = {
+        isConfirmed: false,
+    }
+
+    handleCheckBoxChange = () => {
+        this.setState({
+            isConfirmed: !this.state.isConfirmed,
+            isFormSubmitted: false,
+        })
+    }
+
+    displayMessage = () => {
+        if (this.state.isFormSubmitted) {
+            if (this.state.isConfirmed) {
+                return <PositiveMessage/>
+            } else {
+                return <NegativeMessage/>
+            }
+        } else {
+            return null
+        }
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        if (!this.state.isFormSubmitted)
+            this.setState({
+                isFormSubmitted: true,
+            })
+    }
+
+    render() {
+        console.log(this.state.isConfirmed);
+        return (
+            <>
+                <h1>Kup bilet na horror roku!</h1>
+                <form onSubmit={this.handleFormSubmit}>
+                    <input type="checkbox" id="age"
+                           onChange={this.handleCheckBoxChange}
+                           checked={this.state.isConfirmed}/>
+                    <label htmlFor="age">Mam co najmniej 16 lat</label>
+                    <br/>
+                    <button type="submit">Kup bilet</button>
+                </form>
+                {this.displayMessage()}
+            </>
         )
     }
 }
+
+ReactDOM.render(<TicketShop/>, document.getElementById("root"));
