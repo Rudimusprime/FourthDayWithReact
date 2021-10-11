e9e6d485a3705ac34d074e44a58aae2adafbd752
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-11 20:29:19 +0200

    Initial commit

---
+++ FirstExample/scripts/App.js
@@ -0,0 +1,9 @@
+class CheckBoxConfirmation extends React.Component{
+    redner(){
+        return(
+            <h1>Kup bilet na horror roku!</h1>
+            <input type="checkbox"/>
+        <label hmtlFor=""></label>
+        )
+    }
+}
